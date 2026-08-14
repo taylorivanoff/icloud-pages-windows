4a1c3d0c69f0ac17bd552ba663230d48ecfff686
Taylor Ivanoff <taylorivanoff@gmail.com>
2026-08-14 23:44:59 +0200

    Update iCloud Pages configuration to enable toolbar removal.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,7 @@ pub fn run() {
         tauri_icloud_base::IcloudAppConfig {
             app_name: "iCloud Pages",
             url: "https://www.icloud.com/pages",
-            remove_toolbar: false,
+            remove_toolbar: true,
         },
         tauri::generate_context!(),
     );
